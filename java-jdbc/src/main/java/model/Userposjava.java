56fcfb3b263ca18577f54fb4cb4f92c3e5d34a6c
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-10-12 15:22:23 -0300

    Criando a estrutura de DAO de acesso ao banco de dados

---
+++ java-jdbc/src/main/java/model/Userposjava.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Userposjava {
+
+	private Long ind;
+	private String nome;
+	private String email;
+
+	public Long getInd() {
+		return ind;
+	}
+
+	public void setInd(Long ind) {
+		this.ind = ind;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
